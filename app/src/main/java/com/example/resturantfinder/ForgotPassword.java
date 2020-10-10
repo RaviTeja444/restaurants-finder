67849a667852205c59fe88f1cf6fdf8c39abd157
srkvodnala <srkrao547@gmail.com>
2020-10-09 20:04:36 -0500

    Shiva Rama Krishna Vodnala : java code for forgot password is done.

---
+++ app/src/main/java/com/example/resturantfinder/ForgotPassword.java
@@ -0,0 +1,19 @@
+package com.example.resturantfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class ForgotPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+    }
+    public void goToPasswordRestPage(View v){
+        Intent passwordRestPage=new Intent(this,password_reset.class);
+        this.startActivity(passwordRestPage);
+    }
+}
