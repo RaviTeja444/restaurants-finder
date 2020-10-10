09bf61fda7ea97f05de06c671e4361c47379a465
srkvodnala <srkrao547@gmail.com>
2020-10-09 23:48:53 -0500

    Shiva Rama Krishna Vodnala : Filter Activity

--- app/src/main/java/com/example/resturantfinder/Filter.java
+++ app/src/main/java/com/example/resturantfinder/Filter.java
@@ -12,12 +12,7 @@ public class Filter extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_filter);
     }
-
-    public void goBackToStartUp(View v){
-        onBackPressed();
-        //Intent startUpIntent=new Intent(this,Startup.class);
-        //this.startActivity(startUpIntent);
-    }
+    
 
     public void goToRestaurantListonSearch(View v){
         Intent resListIntent=new Intent(this,FilterRestaurantList.class);
