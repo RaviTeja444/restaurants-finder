4a91a1fcdc54862f439d30ce542cb1324e9cc346
RaviTeja444 <pagidojuraviteja2@gmail.com>
2020-10-09 20:29:13 -0500

    Ravi Teja Pagidoju : Java code implemented for register page.

---
+++ app/src/main/java/com/example/resturantfinder/SignUp.java
@@ -0,0 +1,20 @@
+package com.example.resturantfinder;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+
+    public void goToLoginPage(View v){
+        Intent loginPageIntent = new Intent(this,LoginPage.class);
+        this.startActivity(loginPageIntent);
+    }
+}
