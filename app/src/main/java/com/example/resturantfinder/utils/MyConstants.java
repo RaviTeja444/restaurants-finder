b07557739b8f2e376821785a8f07e7bb2b2f416f
chanduhvg <chanduhvg@gmail.com>
2020-11-19 23:06:40 -0600

    Gopichand Bandarupalli : Added files for Singleton and all configuration values to be fetched.

---
+++ app/src/main/java/com/example/resturantfinder/utils/MyConstants.java
@@ -0,0 +1,52 @@
+package com.example.resturantfinder.utils;
+
+public class MyConstants {
+
+    public static String vacationtourapp="pepper_member";
+    public static String uId="userId";
+    public static String uFirstName="userFirstName";
+    public static String uLastName="userLastName";
+    public static String uEmail="userEmail";
+    public static String uPhone="userPhone";
+    public static String uRole="userRole";
+    public static String FAVLIST="FAVLIST";
+    public static String FAVLIST_SIZE="FAVLIST_SIZE";
+    public static String isFromAdmin="isFromAdmin";
+
+    public static String Restaurant="RestaurantList";
+    public static String RestaurantList_SIZE="RestaurantList_SIZE";
+    public static String FeedBack="FeedBack";
+    public static String restaurantId="restaurantId";
+    public static String restaurantTiming="restaurantTiming";
+    public static String restaurantcontactNo="restaurantcontactNo";
+    public static String restaurantemail="restaurantemail";
+    public static String restaurantfoodStyle="restaurantfoodStyle";
+    public static String restaurantorderlink="restaurantorderlink";
+    public static String restaurantzipCode="zipCode";
+    public static String restaurantState="restaurantState";
+    public static String restaurantName="restaurantName";
+    public static String restaurantCity="restaurantCity";
+    public static String restaurantPositon="position";
+
+    public static String notFav="notFav";
+
+    public static String restaurantDel="restaurantDel";
+    //refrence
+    public static String USERS="users";
+
+    public static int feedbackResult=101;
+    public static int restaurantaddedValue=102;
+
+
+    public static String Admin="Admin";
+    public static String user="user";
+    public static String restaurantValue="restaurantValue";
+    public static String hideFeedback="hideFeedback";
+    public static String filterType="filterType";
+    public static String filterValue="filterValue";
+    public static String feedBackEntered="feedBackEntered";
+    public static String restaurantadded="restaurantadded";
+
+
+
+}
---
+++ app/src/main/java/com/example/resturantfinder/utils/ServerUtils.java
@@ -0,0 +1,9 @@
+package com.example.resturantfinder.utils;
+
+public class ServerUtils {
+
+    public static String baseurl="https://restaurantsfinder-7fab0.firebaseio.com/";
+    public static String restaurantList=baseurl+"RestaurantList.json";
+    public static String favRestaurantList=baseurl+"FAVLIST/";
+
+}
